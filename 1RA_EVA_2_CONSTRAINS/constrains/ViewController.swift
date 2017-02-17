8c5a6b499d013c6b0172e677a14eafa96f41ea63
Mickyssin <micky_moco_@hotmail.com>
2017-02-17 11:57:51 -0700

    Add files via upload

---
+++ 1RA_EVA_2_CONSTRAINS/constrains/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  constrains
+//
+//  Created by MIGUEL ANGEL on 02/02/17.
+//  Copyright © 2017 MIGUEL ANGEL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ 1RA_EVA_3_CONSTRAINS-2/1RA_EVA_3_CONSTRAINS-2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  1RA_EVA_3_CONSTRAINS-2
+//
+//  Created by MIGUEL ANGEL on 03/02/17.
+//  Copyright © 2017 MIGUEL ANGEL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
